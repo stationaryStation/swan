ef1a15ce26cb2bc2f107eed32dbc9d66b93376d2
Raul (Station) Perez <raulytstation@outlook.com>
2021-12-15 12:38:39 -0400

    Navigation Bar (but cool)

---
+++ src/lib/nav.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import './css/nav.css'
+</script>
+
+<div class="navbar">
+    <nav>
+        <ul>
+            <li><a class="active">Home</a></li>
+            <li><a>Events</a></li>
+            <li><a>Team</a></li>
+            <li class="nav-left"><a>Create</a></li>
+        </ul>
+    </nav>
+</div>
