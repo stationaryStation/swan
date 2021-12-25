3fac229548692bf1336b0da9163aff944389b6ad
Raul ('Station') Perez <raulytstation@outlook.com>
2021-12-25 13:02:19 -0400

    Something

--- src/lib/nav.svelte
+++ src/lib/nav.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
     import './css/nav.css'
+    import {links} from "svelte-routing";
     import { HomeIcon, SearchIcon, CalendarIcon, UserIcon, PlusCircleIcon } from 'svelte-feather-icons'
 </script>
 
+<div use:links>
     <nav class="navbar">
         <ul class="navbar-nav">
             <li class="navbar-item"><h3 class="navbar-header">Swan</h3></li>
-            <li class="navbar-item"><a href="#" class="nav-link"><HomeIcon class="nav-icons" size=30px/></a></li>
-            <li class="navbar-item"><a href="#" class="nav-link"><CalendarIcon class="nav-icons" size=30px/></a></li>
-            <li class="navbar-item"><a href="#" class="nav-link"><UserIcon class="nav-icons" size=30px/></a></li>
-            <li class="nav-bottom"><a href="#" class="nav-link"><PlusCircleIcon class="nav-icons" size=30px/></a></li>
+            <li class="navbar-item"><a href="/" class="nav-link"><HomeIcon class="nav-icons" size=30px/></a></li>
+            <li class="navbar-item"><a href="/events" class="nav-link"><CalendarIcon class="nav-icons" size=30px/></a></li>
+            <li class="navbar-item"><a href="/profile" class="nav-link"><UserIcon class="nav-icons" size=30px/></a></li>
+            <li class="nav-bottom"><a href="/create" class="nav-link"><PlusCircleIcon class="nav-icons" size=30px/></a></li>
         </ul>
     </nav>
+</div>
